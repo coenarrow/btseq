214170dd79cada9cd736988166fe222d0b293c93
Coen Arrow <coen.arrow@gmail.com>
2024-10-23 14:51:32 +0800

    added the setup

--- kaya_scripts/run_example.sh
+++ kaya_scripts/run_example.sh
@@ -13,9 +13,12 @@ SCRIPT_DIR=$(pwd)/kaya_scripts
 # set the directory to run. This directory contains the .fastq files and "sample_sheet.txt"
 dir=$1
 
+# First run the setup script which runs the R script, creating the BSTarget_input.txt required for the alignment
+# it does not need to run on slurm
+$SCRIPT_DIR/setup.sh $dir
 # First run prepare_trim on our directory
 jid2=$(sbatch --job-name=prep_trim --output=prep_trim.out --partition='pophealth' $SCRIPT_DIR/prepare_trim.sh $dir)
 jid2=${jid2:20}
-jid3=$(sbatch --job-name=prepare_align --dependency=afterany:$jid2 --partition='pophealth' --output=prep_aln.out $SCRIPT_DIR/prepare_align.sh)
+jid3=$(sbatch --job-name=prepare_align --dependency=afterany:$jid2 --partition='pophealth' --output=prep_aln.out $SCRIPT_DIR/prepare_align.sh $dir)
 jid3=${jid3:20}
 # sbatch --dependency=afterany:$jid3 --job-name=run --output=run.out btseq_run
