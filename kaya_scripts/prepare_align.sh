c500ffb2f963a8950094d63aef5a79bd27aa21d6
Coen Arrow <coen.arrow@gmail.com>
2024-10-23 14:50:55 +0800

    updated prepare_align to properly load the modules

--- kaya_scripts/prepare_align.sh
+++ kaya_scripts/prepare_align.sh
@@ -7,8 +7,11 @@ else
     outdir="$1"
 fi
 
+# This isn't a variable that's used, but it's the path to the modules so they can be loaded
+ENV_MODULE_PATH="/group/pgh004/carrow/repo/btseq/env/modules"
+
 # Set the input file containing the definitions for the job
-defs="${outdir}/sample_sheet.txt"
+defs="${outdir}/BSTarget_input.txt"
 
 # Create a directory named "Run" within the current working directory
 mkdir -p "${outdir}/Run"
@@ -23,45 +26,40 @@ chmod +x "${outdir}/Run/Run.q"
 echo '#! /bin/sh' >> "${outdir}/Run/Run.q"
 
 # Read the input definitions from the file, line by line, and use awk to generate individual job scripts
-# Each job script will be named using the second and first columns of the input file, and will include commands to 
-# execute a series of bioinformatics steps
-awk -v d="${outdir}" '
+awk -v outdir="${outdir}" '
 {
-    # Create a variable Qout that holds the path to the job script for each task
-    Qout=d "/Run/" $2 "_" $1 ".q";
+    # Define the path for the job script to be created
+    Qout=outdir "/Run/" $2 "_" $1 ".q";
     
-    # Create the job script by appending the following commands:
-
     # 1. Create the job script file for the specific task
-    print "touch " d "/Run/" $2 "_" $1 ".q\n" \
+    print "#! /bin/sh" > Qout;  # Write shebang directly into the job script
     
-    # 2. Add the shebang line for the job script
-    "echo \047\043\\\041/bin/sh\047 >> " Qout "\n" \
+    # 2. Create a directory for the task using the second and first columns from the input file
+    print "mkdir -p " outdir "/" $2 "_" $1 >> Qout;
     
-    # 3. Create a directory for the task using the second and first columns from the input file
-    "echo mkdir " d "/" $2 "_" $1 ">> " Qout "\n" \
+    # 3. Add a command to create symbolic links to the input file (fourth column in the input file)
+    print "ln -s " $4 " " outdir "/" $2 "_" $1 >> Qout;
     
-    # 4. Add a command to create symbolic links to the input file (fourth column in the input file)
-    "echo ln -s " $4 " " d "/" $2 "_" $1 " >> " Qout "\n" \
+    # 4. Load the bioinformatics environment modules needed to run the commands (samtools, bowtie2 and bismark)
+    print "export MODULEPATH=/group/pgh004/carrow/repo/btseq/env/modules:$MODULEPATH" >> Qout;
+    print "module load samtools" >> Qout;
+    print "module load bowtie2" >> Qout;
+    print "module load bismark" >> Qout;
     
-    # 5. Load the bioinformatics environment modules needed to run the commands (bioinfo and bio)
-    "echo module load bioinfo >> " Qout "\n" \
-    "echo module load bio >> " Qout "\n" \
+    # 5. Run Bismark genome preparation step using Bowtie2 on the created directory
+    print "bismark_genome_preparation --bowtie2 " outdir "/" $2 "_" $1 >> Qout;
     
-    # 7. Run Bismarks genome preparation step using Bowtie2 on the created directory
-    "echo bismark_genome_preparation --bowtie2 " d "/" $2 "_" $1 " >> " Qout "\n" \
+    # 6. Run the Bismark alignment tool using Bowtie2, with options for SAM output format and setting output directory
+    print "bismark " outdir "/" $2 "_" $1 " " $3 " --sam --bowtie2 -o " outdir "/" $2 "_" $1 "/Output --temp_dir " outdir "/" $2 "_" $1 "/Output" >> Qout;
     
-    # 8. Run the Bismark alignment tool using Bowtie2, with options for SAM output format and setting output directory
-    "echo bismark " d "/" $2 "_" $1 " " $3 " --sam --bowtie2 -o " d "/" $2 "_" $1 "/Output --temp_dir " d "/" $2 "_" $1 "/Output >> " Qout "\n" \
+    # 7. Analyze the result using a custom script (Analyze_Result.csh) for the alignment result
+    print "/cs/icore/joshua.moss/scripts/btseq/scripts/Analyze_Result.csh " $3 " " outdir "/" $2 "_" $1 >> Qout;
     
-    # 9. Analyze the result using a custom script (Analyze_Result.csh) for the alignment result
-    "echo /cs/icore/joshua.moss/scripts/btseq/scripts/Analyze_Result.csh " $3 " " d "/" $2 "_" $1 " >> " Qout "\n" \
-    
-    # 10. Submit the job script to the SLURM scheduler with specified resources (4GB memory, 3 CPUs, 5 hours runtime)
-    # and direct its output log to a corresponding .q.out file
-    "echo sbatch --mem=4GB --cpus-per-task=3 --partition='pophealth' --time=5:00:00 --output=" d "/Run/" $2 "_" $1 ".q.out " d "/Run/" $2 "_" $1 ".q >> " d "/Run/Run.q"
-}' $defs
+    # 8. Add job submission command to the master "Run.q" script
+    print "sbatch --mem=4GB --cpus-per-task=3 --partition=pophealth --time=5:00:00 --output=" outdir "/Run/" $2 "_" $1 ".q.out " Qout >> outdir "/Run/Run.q";
+}' "$defs"
 
+echo "Script created at ${outdir}/Run/Run.q"
 # At the end of the script, you can use the following to zip the results
 # The commented-out code below will:
 # - Extract the unique values from the second column of Def.txt
